83a610e141dd8dabf8dbb6aa8ce1deed8f208537
NadiaDonnabella <167275899+NadiaDonnabella@users.noreply.github.com>
2024-04-17 13:10:29 +0700

    Sales Portofolio Project.sql

--- Sales Portofolio Project.sql
+++ Sales Portofolio Project.sql
@@ -1,52 +1,63 @@
 -- Annual Performance Metrics
 select year, 
-count(distinct order_id) as total_transaction, 
-sum(quantity) as total_prod_sold,
-sum(sales) as total_sales
+      count(distinct order_id) as total_transaction, 
+      sum(quantity) as total_prod_sold,
+      sum(sales) as total_sales
 from sales_dataset
 group by 1
 order by 1;
 
 -- States with Highest and Lowest Sales
-select state, sum(sales) as total_sales
+select state, 
+       sum(sales) as total_sales
 from sales_dataset
 group by 1
 order by 2 desc
 limit 3;
 
-select state, sum(sales) as total_sales
+select state,
+       sum(sales) as total_sales
 from sales_dataset
 group by 1
 order by 2
 limit 3;
 
 -- Top 5 best and worst seller
-select product_category, product, sum(quantity) as total_prod_sold
+select product_category, 
+       product, 
+       sum(quantity) as total_prod_sold
 from sales_dataset
 group by 1, 2
 order by 3 desc
 limit 5;
 
-select product_category, product, sum(quantity) as total_prod_sold
+select product_category, 
+       product, 
+       sum(quantity) as total_prod_sold
 from sales_dataset
 group by 1, 2
 order by 3
 limit 5;
 
 -- Most Frequently Employed Shipping Mode
-select ship_mode, count(distinct order_id) as total
+select ship_mode, 
+       count(distinct order_id) as total
 from sales_dataset
 group by 1
 order by 2 desc;
 
 -- Customer Count per Segment
-select segment, count(distinct customer_id) as total_customer
+select segment, 
+       count(distinct customer_id) as total_customer
 from sales_dataset
 group by 1
 order by 2;
 
 -- Loyal Customers Based on Transaction History and Purchasing Trends
-select customer_name, state, count(distinct order_id) as total_transaction, sum(sales) as total_purchase
+select customer_name, 
+       state, 
+       count(distinct order_id) as total_transaction, 
+       sum(sales) as total_purchase
 from sales_dataset
 group by 1, 2
 order by 3 desc, 4 desc
